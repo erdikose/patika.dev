1d547d1d600bc0d2ba4bb3a5a6195e7ec506e0bd
Erdi Köse <84688029+erdikose@users.noreply.github.com>
2021-07-08 23:00:10 +0300

    Update odev1.sql

--- odev1.sql
+++ odev1.sql
@@ -19,5 +19,5 @@ WHERE first_name = 'Mary';
 -- Smith
 
 --5. film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
-SELECT length, rental_rate FROM film
+SELECT * FROM film
 WHERE NOT length > 50 and NOT (rental_rate = 2.99 OR rental_rate = 4.99);
